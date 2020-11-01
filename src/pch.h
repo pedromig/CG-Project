4c759030879bf3206cf43da3fcc6e959c1d8a9fa
pedromig <pedromiguelrodrigues2000@gmail.com>
2020-11-01 22:50:54 +0000

    First Version of the door

---
+++ src/pch.h
@@ -0,0 +1,26 @@
+#ifndef __CG_PROJECT_PCH
+#define __CG_PROJECT_PCH
+
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+// Standard Colors
+#define BLUE 0.0f, 0.0f, 1.0f, 1.0f
+#define RED 1.0f, 0.0f, 0.0f, 1.0f
+#define YELLOW 1.0f, 1.0f, 0.0f, 1.0f
+#define GREEN 0.0f, 1.0f, 0.0f, 1.0f
+#define ORANGE 1.0f, 0.5f, 0.1f, 1.0f
+#define CYAN 0.0f, 1.0f, 1.0f, 1.0f
+#define WHITE 1.0f, 1.0f, 1.0f, 1.0f
+#define BLACK 0.0f, 0.0f, 0.0f, 1.0f
+#define GRAY 0.3f, 0.3f, 0.3f, 1.0f
+#define BROWN 0.6f, 0.3f, 0.0f, 1.0f
+
+#endif // __CG_PROJECT_PC
